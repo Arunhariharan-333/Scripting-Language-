0b464b2eb4566fba2e6431006cca18bb09fb0f40
Arunhariharan-333 <arunhariharan.333@gmail.com>
2024-11-26 12:59:12 +0530

    Create Palindrome.js

---
+++ Activity/Palindrome.js
@@ -0,0 +1,9 @@
+function isPalindrome(x) {
+    if (x < 0) return false;
+    const str = x.toString();
+    return str === str.split('').reverse().join('');
+}
+
+console.log(isPalindrome(121));  
+console.log(isPalindrome(-121)); 
+console.log(isPalindrome(10)); 
